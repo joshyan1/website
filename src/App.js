3dfd3126eb49e87444400a7b3711d48f6a8f0c4b
Josh Yan <jyan00017@gmail.com>
2024-07-07 16:32:00 -0700

    set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import TerminalComponent from './components/Terminal';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <div className="App h-screen flex justify-center items-center bg-black">
       <TerminalComponent />
+      <SpeedInsights />
     </div>
   );
 }
